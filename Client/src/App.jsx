eb864db44e0dd3440b8360beb3be04d801179d20
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-11 01:52:12 +0530

    Initial commit - CineSeat Client Side

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <div className="group text-center">
+        <h1 className="text-primary group-hover:text-primary-dull text-7xl font-semibold px-6 py-3 rounded-lg transition-colors">
+          CineSeat
+        </h1>
+
+        <h2 className="text-primary-dull group-hover:text-primary text-7xl font-semibold px-6 py-3 rounded-lg transition-colors">
+          App
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default App;
