7b123c33852c6975eb68fbcbd4ff54da89622f4b
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-11 05:20:45 +0530

    Completed FeaturedSection Component

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 // CineSeat/Client/src/pages/Home.jsx
 import React from "react";
 import HeroSection from "../components/HeroSection";
+import FeaturedSection from "../components/FeaturedSection";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <FeaturedSection />
     </>
   );
 };
