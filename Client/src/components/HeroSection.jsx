9f93fa7b0a9121483af65863d84cf4a8e7e173b3
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-11 04:55:29 +0530

    Completed Hero Section

---
+++ Client/src/components/HeroSection.jsx
@@ -0,0 +1,57 @@
+// CineSeat/Client/src/components/HeroSection.jsx
+import React from "react";
+import { assets } from "../assets/assets";
+import { ArrowRight, CalculatorIcon, ClockIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+
+const HeroSection = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/BackgroundImage.jpg")] bg-cover bg-center h-screen'>
+      <img src={assets.Ags} alt="" className="max-h-11 lg:h-11 mt-20" />
+      <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110">
+        The Greatest of <br />
+        All Time
+      </h1>
+
+      <div className="flex items-center gap-4 text-gray-300">
+        <span>Action | Sci-Fi | Drama</span>
+        <div className="flex items-center gap-1">
+          <CalculatorIcon className="w-4.5 h-4.5" /> 2024
+        </div>
+
+        <div className="flex items-center gap-1">
+          <ClockIcon className="w-4.5 h-4.5" /> 3h 3m
+        </div>
+      </div>
+
+      <p className="max-w-md text-gray-300">
+        A high-octane sci-fi thriller where a retired agent returns for one last
+        mission that could change the course of humanity. Packed with futuristic
+        action, emotion, and Vijay’s powerful performance.
+      </p>
+
+      {/* <button
+        onClick={() => navigate("/movies")}
+        className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+      >
+        Explore Movies <ArrowRight className="w-5 h-5" />
+      </button> */}
+
+      <Button
+        text={
+          <span className="flex items-center gap-1">
+            Explore Movies <ArrowRight className="w-5 h-5" />
+          </span>
+        }
+        onClick={() => navigate("/movies")}
+        variant="primary"
+        // className="px-6 py-3 text-sm"
+      />
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// CineSeat/Client/src/pages/Home.jsx
+import React from "react";
+import HeroSection from "../components/HeroSection";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+};
+
+export default Home;
