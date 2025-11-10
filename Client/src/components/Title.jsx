e32b19bb8e3a2502337a7a9c4d5d05616be0a544
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-11 04:26:31 +0530

    Completed Title Component

---
+++ Client/src/components/Title.jsx
@@ -0,0 +1,16 @@
+// CineSeat/Client/src/components/Title.jsx
+import React from "react";
+
+const Title = ({ text1, text2 }) => {
+  return (
+    <div className="inline-flex gap-2 items-center mb-0">
+      <p className="text-primary text-2xl md:text-3xl font-medium">
+        {text1} <span className="text-black font-medium">{text2}</span>
+      </p>
+
+      <p className="w-8 sm:w-12 h-px sm:h-0.5 bg-primary-dull"></p>
+    </div>
+  );
+};
+
+export default Title;
