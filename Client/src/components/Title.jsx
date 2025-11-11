6dc92a193982ea07101b1159d1670daabf599b74
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-11 15:33:40 +0530

    Updated Title Component

--- Client/src/components/Title.jsx
+++ Client/src/components/Title.jsx
@@ -1,14 +1,30 @@
 // CineSeat/Client/src/components/Title.jsx
 import React from "react";
 
-const Title = ({ text1, text2 }) => {
+const getTextColor = (index) =>
+  index % 2 === 0 ? "text-primary" : "text-white";
+
+const getLineColor = (numTexts) =>
+  numTexts % 2 === 0 ? "bg-primary-dull" : "bg-white";
+
+const Title = ({ ...texts }) => {
+  const textValues = Object.values(texts);
+
   return (
     <div className="inline-flex gap-2 items-center mb-0">
-      <p className="text-primary text-2xl md:text-3xl font-medium">
-        {text1} <span className="text-black font-medium">{text2}</span>
+      <p className="text-2xl md:text-3xl font-medium">
+        {textValues.map((text, i) => (
+          <span key={i} className={`${getTextColor(i)} font-medium`}>
+            {text}{" "}
+          </span>
+        ))}
       </p>
 
-      <p className="w-8 sm:w-12 h-px sm:h-0.5 bg-primary-dull"></p>
+      <p
+        className={`w-8 sm:w-12 h-px sm:h-0.5 ${getLineColor(
+          textValues.length
+        )}`}
+      ></p>
     </div>
   );
 };
