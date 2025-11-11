ddc337c4f6856b1a81e5dcb5df3bd1131ba8754d
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-11 20:56:55 +0530

    Completed dateFormat utility

---
+++ Client/src/lib/dateFormat.js
@@ -0,0 +1,10 @@
+// CineSeat/Client/src/lib/dateFormat.js
+export const dateFormat = (date) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    weekday: "short",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+};
