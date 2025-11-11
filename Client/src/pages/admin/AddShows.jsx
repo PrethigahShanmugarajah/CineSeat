9ab706af863cc80630856d87479b6caa8cbc651e
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-12 01:19:31 +0530

    Completed AddShows Page

--- Client/src/pages/admin/AddShows.jsx
+++ Client/src/pages/admin/AddShows.jsx
@@ -1,4 +1,4 @@
-// CineSeat/Client/src/admin/client/AddShows.jsx
+// CineSeat/Client/src/pages/admin/AddShows.jsx
 import React, { useEffect, useState } from "react";
 import { dummyShowsData } from "../../assets/assets";
 import Loading from "../../components/Loading";
