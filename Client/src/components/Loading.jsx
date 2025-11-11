de6885f03b6d5c9f27f61ec52497fa755d5ea747
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-11 19:41:38 +0530

    Completed Loading Component

---
+++ Client/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+// CineSeat/Client/src/components/Loading.jsx
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-[80vh]">
+      <div className="animate-spin rounded-full h-24 w-24 border-2 border-t-primary"></div>
+    </div>
+  );
+};
+
+export default Loading;
