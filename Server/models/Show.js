e2cae5bef590c26ccb18c732d5f30fc8a36ee0aa
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-15 15:25:23 +0530

    Added Show model

---
+++ Server/models/Show.js
@@ -0,0 +1,16 @@
+// CineSeat / Server / models / Show.js
+import mongoose from "mongoose";
+
+const showSchema = new mongoose.Schema(
+  {
+    movie: { type: String, required: true, ref: "Movie" },
+    showDateTime: { type: Date, required: true },
+    showPrice: { type: Number, required: true },
+    iccupiedSeats: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
+
+const Show = mongoose.model("Show", showSchema);
+
+export default Show;
