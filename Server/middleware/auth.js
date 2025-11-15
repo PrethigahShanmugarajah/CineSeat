41e8d50c4536c282470b491ebf96034e5352f437
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-15 17:14:02 +0530

    Added protectAdmin middleware for admin authorization

---
+++ Server/middleware/auth.js
@@ -0,0 +1,20 @@
+// CineSeat / Server / middleware / auth.js
+import { clerkClient } from "@clerk/express";
+
+export const protectAdmin = async (req, res, next) => {
+  try {
+    const { userId } = req.auth();
+
+    const user = await clerkClient.users.getUser(userId);
+
+    if (user.privateMetadata.role !== "admin") {
+      return res
+        .status(403)
+        .json({ success: false, message: "Not Authorized" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ success: false, message: "Not Authorized" });
+  }
+};
