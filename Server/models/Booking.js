2c2def303f35971f2fd2f7f77355b2fc02491611
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-15 18:08:20 +0530

    Added Booking model

---
+++ Server/models/Booking.js
@@ -0,0 +1,18 @@
+// CineSeat / Server / models / Booking.js
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: { type: String, required: true, ref: "User" },
+    show: { type: String, required: true, ref: "Show" },
+    amount: { type: Number, required: true },
+    bookedSeats: { type: Array, required: true },
+    isPaid: { type: Boolean, required: true },
+    paymentLink: { type: String },
+  },
+  { timestamps: true }
+);
+
+const Booking = mongoose.model("Boooking", bookingSchema);
+
+export default Booking;
