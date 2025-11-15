121a0ae562e1ca9b707057e22c75204776b87cf6
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-15 18:08:57 +0530

    Updated Show model

--- Server/models/Show.js
+++ Server/models/Show.js
@@ -6,7 +6,7 @@ const showSchema = new mongoose.Schema(
     movie: { type: String, required: true, ref: "Movie" },
     showDateTime: { type: Date, required: true },
     showPrice: { type: Number, required: true },
-    iccupiedSeats: { type: Object, default: {} },
+    occupiedSeats: { type: Object, default: {} },
   },
   { minimize: false }
 );
