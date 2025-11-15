d2728877fc23d8a772187fbf3e488edecffc8c10
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-15 18:32:54 +0530

    Created Create Booking Function

--- Server/controllers/bookingController.js
+++ Server/controllers/bookingController.js
@@ -1,8 +1,9 @@
 // CineSeat / Server / controllers / bookingController.js
-import Show from "../models/Show";
+import Booking from "../models/Booking.js";
+import Show from "../models/Show.js";
 
 /* -------- CHECK AVAILABILITY OF SELECTED SEATS FOR A MOVIE -------- */
-export const checkSeatsAvailbility = async (showId, selectedSeats) => {
+export const checkSeatsAvailability = async (showId, selectedSeats) => {
   try {
     const showData = await Show.findById(showId);
     if (!showData) return false;
@@ -19,5 +20,56 @@ export const checkSeatsAvailbility = async (showId, selectedSeats) => {
     );
 
     return false;
+  }
+};
+
+/* -------- CREATE BOOKING -------- */
+export const createBooking = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { showId, selectedSeats } = req.body;
+    const { origin } = req.headers;
+
+    // Check if the Seat is Available for the Selected Show
+    const isAvailable = await checkSeatsAvailability(showId, selectedSeats);
+
+    if (!isAvailable) {
+      return res.status(400).json({
+        success: false,
+        message: "Selected Seats are not Available",
+      });
+    }
+
+    // Get the show Details
+    const showData = await Show.findById(showId).populate("movie");
+
+    // Create a new Booking
+    const booking = await Booking.createBooking({
+      user: userId,
+      show: showId,
+      amount: showData.showPrice * selectedSeats.length,
+      bookedSeats: selectedSeats,
+    });
+
+    selectedSeats.map((seat) => {
+      showData.occupiedSeats[seat] = userId;
+    });
+
+    showData.markModified("occupiedSeats");
+
+    await showData.save();
+
+    // Stripe Gateway Initialize
+    return res.status(200).json({
+      success: true,
+      message: "Booked Successfully",
+    });
+  } catch (error) {
+    console.error("Create Booking Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: `Create Booking Error: ${error.message}`,
+    });
   }
 };
---
+++ Server/routes/bookingRoutes.js
@@ -0,0 +1,9 @@
+// CineSeat / Server / routes / bookingRoutes.js
+import express from "express";
+import { createBooking } from "../controllers/bookingController.js";
+
+const bookingRouter = express.Router();
+
+bookingRouter.post("/create", createBooking);
+
+export default bookingRouter;
