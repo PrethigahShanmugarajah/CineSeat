50e53e7a0eba1f62bccd41fa3eb1d82cbe7e1fce
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-15 20:40:10 +0530

    Updated Loading Component

--- Client/src/components/Loading.jsx
+++ Client/src/components/Loading.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-[80vh]">
-      <div className="animate-spin rounded-full h-24 w-24 border-2 border-t-primary"></div>
+    <div className="flex items-center justify-center h-[80vh]">
+      <div className="animate-spin rounded-full h-24 w-24 border-t-10 border-primary border-solid"></div>
     </div>
   );
 };
