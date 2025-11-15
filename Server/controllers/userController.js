28da43945a9cd30db5c8b9582f47b1c6268b7714
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-15 20:07:51 +0530

    Created Update Favorite Movie in Clerk User MetaData Function

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,6 +1,7 @@
 // CineSeat / Server / controllers / userController.js
 import { clerkClient } from "@clerk/express";
 import Booking from "../models/Booking.js";
+import User from "../models/User.js";
 
 /* -------- GET USER BOOKINGS -------- */
 export const getUserBookings = async (req, res) => {
@@ -58,6 +59,47 @@ export const addFavorite = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: `Add Favorite Movie in Clerk User MetaData Error: ${error.message}`,
+    });
+  }
+};
+
+/* -------- UPDATE FAVORITE MOVIE IN CLERK USER METADATA -------- */
+export const updateFavourite = async (req, res) => {
+  try {
+    const { movieId } = req.body;
+    const userId = req.auth().userId;
+
+    const user = await clerkClient.users.getUser(userId);
+
+    if (!user.privateMetadata.favorites) {
+      user.privateMetadata.favorites = [];
+    }
+
+    if (!user.privateMetadata.favorites.includes(movieId)) {
+      user.privateMetadata.favorites.push(movieId);
+    } else {
+      user.privateMetadata.favorites = user.privateMetadata.favorites.filter(
+        (item) => item !== movieId
+      );
+    }
+
+    await clerkClient.users.updateUserMetadata(userId, {
+      privateMetadata: user.privateMetadata,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Favourite Movie Updated Successfully",
+    });
+  } catch (error) {
+    console.error(
+      "Update Favorite Movie in Clerk User MetaData Error:",
+      error.message
+    );
+
+    return res.status(500).json({
+      success: false,
+      message: `Update Favorite Movie in Clerk User MetaData Error: ${error.message}`,
     });
   }
 };
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,10 +1,15 @@
 // CineSeat / Server / routes / userRoutes.js
 import express from "express";
-import { addFavorite, getUserBookings } from "../controllers/userController.js";
+import {
+  addFavorite,
+  getUserBookings,
+  updateFavourite,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/bookings", getUserBookings);
 userRouter.post("/add-favorite", addFavorite);
+userRouter.post("/update-favorite", updateFavourite);
 
 export default userRouter;
